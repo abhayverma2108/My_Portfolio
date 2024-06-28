273a531aaa30398d230d2d07a0e74e554501f256
Abhay Verma <abhayv175@gmail.com>
2024-06-28 18:47:07 +0530

    fast

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import Contact from './sections/Contact/Contact.jsx';
-import Footer from './sections/Footer/Footer';
-import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
-import Skills from './sections/Skills/Skills';
+import Footer from './sections/Footer/Footer.jsx';
+import Hero from './sections/Hero/Hero.jsx';
+import Projects from './sections/Projects/Projects.jsx';
+import Skills from './sections/Skills/Skills.jsx';
 
 function App() {
   return (
