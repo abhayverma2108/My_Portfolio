56eb4309f9e8309ee7ef2a994eef481dd26ac075
Abhay Verma <abhayv175@gmail.com>
2024-06-28 19:03:53 +0530

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
-import Footer from 'src/sections/Footer/Footer.jsx';
-import Contact from 'src/sections/Contact/Contact.jsx';
-import Hero from 'src/sections/Hero/Hero';
-import Projects from 'src/sections/Projects/Projects';
-import Skills from 'src/sections/Skills/Skills';
+import Footer from './sections/Footer/Footer';
+import Contact from './sections/Contact/Contact';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
 
 function App() {
   return (
