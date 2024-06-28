97518d53c75b581568e7ed3366c69e4d07716815
Abhay Verma <abhayv175@gmail.com>
2024-06-28 18:51:51 +0530

    minor tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import Contact from './sections/Contact/Contact.jsx';
 import Footer from './sections/Footer/Footer.jsx';
+import Contact from './sections/Contact/Contact.jsx';
 import Hero from './sections/Hero/Hero.jsx';
 import Projects from './sections/Projects/Projects.jsx';
 import Skills from './sections/Skills/Skills.jsx';
