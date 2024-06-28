16681008a4f80e3fe5ceaee6fbd05726ec2edec7
Abhay Verma <abhayv175@gmail.com>
2024-06-28 18:24:15 +0530

    test

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -1,53 +1,10 @@
-import { useState } from 'react';
-import emailjs from "@emailjs/browser"
 import styles from './ContactStyles.module.css';
 
 function Contact() {
-   const [formData, setFormData] = useState({
-     name: "",
-     email: "",
-     message: "",
-   });
-
-   const [submitted, setSubmitted] = useState(false);
-
-   const handleChange = (event) => {
-     const { name, value } = event.target;
-     setFormData({
-      ...formData,
-      [name]: value,
-     });
-   };
-
-   const handleSubmit = (event) => {
-    event.preventDefault();
-
-    console.log("submit formData=", formData);
-
-    emailjs
-    .send(
-      import.meta.env.VITE_SERVICE_ID,
-      import.meta.env.VITE_TEMPLATE_ID,
-      formData,
-      import.meta.env.VITE_API_KEY
-    )
-    .then(
-      (response) => {
-        console.log("SUCCESS!", response.status, response.text);
-        setSubmitted(true);
-      },
-      (error) => {
-        console.log("FAILED...", error);
-      }
-    );
-
-
-   };
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      {submitted && <div>Thank you for your message!</div>}
-      <form action="" onSubmit={handleSubmit}>
+      <form action="">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -58,8 +15,6 @@ function Contact() {
             id="name"
             placeholder="Name"
             required
-            value={formData.name}
-            onChange={handleChange}
           />
         </div>
         <div className="formGroup">
@@ -72,8 +27,6 @@ function Contact() {
             id="email"
             placeholder="Email"
             required
-            value={formData.email}
-            onChange={handleChange}
           />
         </div>
         <div className="formGroup">
@@ -84,10 +37,7 @@ function Contact() {
             name="message"
             id="message"
             placeholder="Message"
-            required
-            value={formData.message}
-            onChange={handleChange}
-            ></textarea>
+            required></textarea>
         </div>
         <input className="hover btn" type="submit" value="Submit" />
       </form>
